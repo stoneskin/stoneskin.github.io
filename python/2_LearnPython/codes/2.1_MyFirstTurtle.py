7298b7d36564a97964b36d2b6a871e6cfb0f9e1e
wsun <wsun@ipipeline.com>
2020-10-11 14:02:59 -0400

    update

--- python/2_LearnPython/codes/2.1_MyFirstTurtle.py
+++ python/2_LearnPython/codes/2.1_MyFirstTurtle.py
@@ -1,19 +1,28 @@
 # Code sample MyFirstTurtle.py
 import turtle,time
 t = turtle.Pen()
+t2 = turtle.Pen()
 
 # Draw line with length 100
 t.forward(100)
+t2.forward(100)
 
 # make turn 90 degree and draw another line
 t.left(90)
 t.forward(100)
+t2.right(90)
+t2.forward(100)
 
 # continue turn and drawing line twice
 t.left(90)
 t.forward(100)
 t.left(90)
 t.forward(100)
+
+t2.right(90)
+t2.forward(100)
+t2.right(90)
+t2.forward(100)
 
 # waiting 10 sec and exit python running window
 time.sleep(10)
