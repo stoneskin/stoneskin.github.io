36acc5fce2b446800ae7d6d87abfe783875ec2d8
wsun <wsun@ipipeline.com>
2021-03-21 16:52:12 -0400

    update checker

--- python/2_LearnPython/codes/TicTacToe/board.py
+++ python/2_LearnPython/codes/TicTacToe/board.py
@@ -1,3 +1,4 @@
+from checker import CheckerO,CheckerX
 import pygame
 
 class Board:
@@ -5,6 +6,11 @@ class Board:
     def __init__(self,x:float,y:float,width:int,height:int) -> None:
         self.pos:list[float]=[x,y]
         self.img=pygame.transform.scale(self.img, (width, height))
+        self.checker1=CheckerO(100,100)
+        self.checker2=CheckerX(300,100)
         
     def display(self,screen) ->None:
         screen.blit(self.img, self.pos)
+        self.checker1.display(screen)
+        self.checker2.display(screen)
+    
--- python/2_LearnPython/codes/TicTacToe/checker.py
+++ python/2_LearnPython/codes/TicTacToe/checker.py
@@ -1,3 +1,29 @@
-class Checker:
-    def __init__(self) -> None:
-        pass
+import pygame
+
+class Checker:    
+    # value:str
+    # img:any
+    # position:tuple(int,int)    
+    def __init__(self,val:str,img:any,pos:tuple) -> None:
+        self.value=val
+        self.img=pygame.transform.scale(img, (100,100))
+        self.position=pos
+    
+    def display(self,screen) ->None:
+        screen.blit(self.img, self.position)
+    
+    def getValue(self) ->str:
+        return self.value
+    
+class CheckerO(Checker):
+    imgO:any=pygame.image.load("checker_O.png")
+    
+    def __init__(self,x,y):
+        
+        super().__init__("O",self.imgO,(x,y)) 
+        
+
+class CheckerX(Checker):
+    imgX:any=pygame.image.load("checker_X.png") 
+    def __init__(self,x,y):
+        super().__init__("X",self.imgX,(x,y)) 
